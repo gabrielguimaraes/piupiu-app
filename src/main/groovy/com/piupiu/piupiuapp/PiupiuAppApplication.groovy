e4d6d14a00edf7201ef361bc61eddd668a3adc49
Gabriel Guimaraes <gabguim@gmail.com>
2024-03-12 21:21:06 +0100

    Piupiu App first commit

---
+++ src/main/groovy/com/piupiu/piupiuapp/PiupiuAppApplication.groovy
@@ -0,0 +1,13 @@
+package com.piupiu.piupiuapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PiupiuAppApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PiupiuAppApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/piupiu/piupiuapp/PiupiuAppApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.piupiu.piupiuapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PiupiuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
